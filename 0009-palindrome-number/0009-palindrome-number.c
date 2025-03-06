700e55131216261e816409b252940fd164a0f996
HARJIT MAHENDRAN <157561535+HARJIT-M@users.noreply.github.com>
2025-03-06 09:55:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,18 @@
+bool isPalindrome(int x) {
+    int n=x;
+    int sum=0;
+    while(n>0)
+    {
+        int a=n%10;
+        sum=(sum*10)+a;
+        n=n/10;
+    }
+    if(sum==x)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
