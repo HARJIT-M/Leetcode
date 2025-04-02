d6b831ccc6dde475728edaa3a49988de77b82720
HARJIT MAHENDRAN <157561535+HARJIT-M@users.noreply.github.com>
2025-04-02 16:21:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (40.68%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -0,0 +1,10 @@
+bool isPerfectSquare(int num) {
+    if (num < 0) return false;
+    
+    for (long int i = 0; i * i <= num; i++) {
+        if (i * i == num) {
+            return true;
+        }
+    }
+    return false;
+}
