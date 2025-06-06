c64a58cbc10566b95c4d77878cde2450c7bf9de7
HARJIT MAHENDRAN <157561535+HARJIT-M@users.noreply.github.com>
2025-06-06 10:02:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (47.89%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isBoomerang(self, points):
+        x1,y1=points[0]
+        x2,y2=points[1]
+        x3,y3=points[2]
+        area = 0.5 * abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))
+        if area==0:
+            return False
+        else:
+            return True
+        
